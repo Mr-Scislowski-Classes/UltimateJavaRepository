8b55cdc458d78b7c5ed29fa61e814d870890644e
Steve Ta <24hongsteveta@usd266.com>
2024-01-10 10:02:09 -0600

    update

--- Unit01/Topic1_3/Main.java
+++ Unit01/Topic1_3/Main.java
@@ -1,8 +1,16 @@
 package Unit01.Topic1_3;
 
 public class Main {
-  public static void main(String[] args) {
-    // Your code goes here
+ 
+  public void move(String name, boolean direction, int number){
+    System.out.println(name + " moves " + direction + " " + number + " step(s)");
+  }
+  public static void main(String[] args){
+      Main p1 = new Main();
+      p1.move("James", boolean, 5);
+
+      Main p2 = new Main();
+      p2.move()
+    }
   }
   
-}
---
+++ Unit01/Topic1_3/Person.java
@@ -0,0 +1,5 @@
+package Unit01.Topic1_3;
+
+public class Person {
+    
+}
