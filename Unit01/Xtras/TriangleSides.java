937cde7f31177fed722329e3dd4908694c4a0067
Steve Ta <24hongsteveta@usd266.com>
2024-01-12 09:26:01 -0600

    Updated Work

--- Unit01/Xtras/TriangleSides.java
+++ Unit01/Xtras/TriangleSides.java
@@ -2,6 +2,13 @@ package Unit01.Xtras;
 
 public class TriangleSides {
     public static void main(String[] args) {
-
+        int a = Integer.parseInt(args [0]);
+        int b = Integer.parseInt(args [1]);
+        int c = Integer.parseInt(args [2]);
+        if ((a + b < c) || (a + c < b) || (b + c < a) ){
+            System.out.println("false");
+        } else{
+            System.out.println("true");
+        }
     }
 }
