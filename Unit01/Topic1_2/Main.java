ad21b39235d4f4ea555a069091dec8f2c1e1f4cb
ElliottRileyStanley <27elliottstanley@usd266.com>
2024-01-23 09:02:43 -0600

    not yellow anymore

--- Unit01/Topic1_2/Main.java
+++ Unit01/Topic1_2/Main.java
@@ -2,10 +2,6 @@ package Unit01.Topic1_2;
 
 public class Main {
   public static void main(String[] args) {
-    int marblesInJar;
-    double averageTestScore;
-    boolean sugarInDrink;
-    final int tires;
   }
   
 }
