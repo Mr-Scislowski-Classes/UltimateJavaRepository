e5fe67c3e43b5b70d3c82d512d3dc372c26fcd9f
Steve Ta <24hongsteveta@usd266.com>
2024-02-01 09:58:14 -0600

    hu

--- Unit03/Topic3_5/Main.java
+++ Unit03/Topic3_5/Main.java
@@ -2,7 +2,8 @@ package Unit03.Topic3_5;
 
 public class Main {
   public static void main(String[] args) {
-
+    SpinnerGame player = new SpinnerGame();
+    player.spin()
   }
 
 }
---
+++ Unit03/Topic3_5/SpinnerGame.java
@@ -0,0 +1,13 @@
+public class SpinnerGame {
+    public int spin(int min, int max) {
+        return 1;
+    }
+
+    public int playRound() {
+        int player = spin(0, 8);
+        
+
+
+        return 1;
+    }
+}
