cef522d3290f00548fd00534d35ba3bc6ee95e49
Steve Ta <24hongsteveta@usd266.com>
2024-02-02 10:21:45 -0600

    Xtras completion

--- Unit03/Xtras/DiscordTagGenerator.java
+++ Unit03/Xtras/DiscordTagGenerator.java
@@ -1,8 +1,19 @@
 package Unit03.Xtras;
+import java.util.Scanner;
 
 public class DiscordTagGenerator {
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
 
+        System.out.println("Please enter your name");
+        String name = s.nextLine();
+
+        int a = (int)(Math.random() * (9999-1000+1)) + 1000;
+
+        String id = Integer.toString(a);
+
+        System.out.println(name + "#" + id);
+        s.close();
     }
 }
 // javac Unit03\Xtras\DiscordTagGenerator.java
--- Unit03/Xtras/QuadraticSolver.java
+++ Unit03/Xtras/QuadraticSolver.java
@@ -1,8 +1,29 @@
 package Unit03.Xtras;
+import java.util.Scanner;
 
 public class QuadraticSolver {
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Please enter the a coefficient: ");
+        double a = s.nextDouble();
+        System.out.println("Please enter the b coefficient: ");
+        double b = s.nextDouble();
+        System.out.println("Please enter the c coefficient: ");
+        double c = s.nextDouble();
+        double result1;
+        double result2;
+        if (Math.sqrt(Math.pow(b, 2) - (4 * a * c)) >0){
+            result1 = (- b + Math.sqrt(Math.pow(b, 2)) - (4 * a * c))/(2 * a);
+            result2 = (- b - Math.sqrt(Math.pow(b, 2)) - (4 * a * c))/(2 * a);
+            System.out.println("The solutions to " + a + "x^2 + " + b + "x + "+ c + " are" +" x=" + result1 + " 5and" +" x=" + result2);
 
+        } else if (Math.sqrt(Math.pow(b, 2) - (4 * a * c)) == 0){
+            result1 = (- b)/ (2 *a);
+            System.out.println("The only solution to " + a + "x^2 + " + b + "x + "+ c + " is" +" x=" + result1);
+        } else{
+            System.out.println("The equation " + a + "x^2 + " + b + "x + "+ c + " has no real solutions");
+        }
+        s.close();
     }
 }
 // javac Unit03\Xtras\QuadraticSolver.java
