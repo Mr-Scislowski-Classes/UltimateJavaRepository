182179ef1d25602cb844dc20d3aa78160ea2da7a
Daniel Scislowski <mr.scislowski@gmail.com>
2024-02-06 10:13:19 -0600

    fix compile/run commands

--- Unit04/Topic4_5InformalCodeAnalysis.java
+++ Unit04/Topic4_5InformalCodeAnalysis.java
@@ -4,5 +4,5 @@ public class Topic4_5InformalCodeAnalysis {
 
 }
 
-// javac Unit04\Topic4_5InformalCodeAnalysis.java; java
-// Unit04.Topic4_5InformalCodeAnalysis
+// javac Unit04\Topic4_5InformalCodeAnalysis.java
+// java Unit04.Topic4_5InformalCodeAnalysis
