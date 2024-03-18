118fa8b111f50f032d82a6dfece8c70cbaf8cc6d
Daniel Scislowski <mr.scislowski@gmail.com>
2024-03-18 14:34:48 -0500

    documented gettingStarted

---
+++ ProgrammingProjects/TextSimilarity/GettingStarted.java
@@ -0,0 +1,27 @@
+package ProgrammingProjects.TextSimilarity;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import org.apache.commons.text.similarity.FuzzyScore;
+import org.apache.commons.text.similarity.LevenshteinDetailedDistance;
+
+public class GettingStarted {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter your first word: ");
+        String first = scanner.nextLine();
+        System.out.println("Please enter your second word: ");
+        String second = scanner.nextLine();
+
+        FuzzyScore f1 = new FuzzyScore(Locale.getDefault());
+
+        System.out.println(
+                "The Fuzzy Score between '" + first + "' and '" + second + "' is: " + f1.fuzzyScore(first, second));
+
+        LevenshteinDetailedDistance l1 = new LevenshteinDetailedDistance();
+        System.out.println("The Levenshtein Distance between '" + first + "' and '" + second + "' is: "
+                + l1.apply(first, second).getDistance());
+    }
+}
---
+++ ProgrammingProjects/TextSimilarity/README.md
@@ -0,0 +1,22 @@
+# Text Similarity
+
+There are many ways to measure how similar two pieces of text are. In this assignment we will explore two algorithms: the Levenshtein distance and the Fuzzy score. We will use the Apache Commons Text library to do this.
+
+## Getting used to the library
+
+1. `GettingStarted.java`
+
+Write a program that uses `Scanner` and asks the use to type in two words. It should then display the `FuzzyScore` between those two words, and the `LevenshteinDistance` between those two words.
+
+For example:
+
+```
+Please enter your first word:
+participate
+Please enter your second word:
+articulate
+The Fuzzy Score between 'participate' and 'articulate' is: 13
+The Levenshtein Distance between 'participate' and 'articulate' is: 3
+```
+
+2. `UsingDictionary.java`
